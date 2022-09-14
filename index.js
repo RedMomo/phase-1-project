eab3c88f3422e9b52c05e29c3a0d5234f1ca24db
Maria Ameen <ameen.maria.s@gmail.com>
2022-09-14 09:10:53 -0400

    update .

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ let surpriseMe = document.getElementById("surprise-me")
 surpriseMe.innerText = "Surprise Me!"
 surpriseMe.addEventListener("click", (e) => {
     e.preventDefault()
+    
     fetch("https://api.punkapi.com/v2/beers/random")
     .then(res => res.json())
     .then(beers => {
         console.log(beers[0].name, beers[0].description)
-    })
+        beers.forEach(beer => {
+            randomBeers(beers) 
         })
+    })
 
+    let randomBeers = (beer) => {
+    const randomBeerContainer = document.getElementById("random-beer-container")
+    const randomBeerCard = document.createElement("div")
+        randomBeerCard.classList.add("random-beer-card")
+        randomBeerCard.innerText = beer[0].name
+        randomBeerContainer.append(randomBeerCard)
 
+        randomBeerCard.addEventListener("mouseover", showCard)
+        randomBeerCard.addEventListener("mouseleave", blurCard)
+            }
+});
+        
 let searchBar = document.getElementById("search-form")
 searchBar.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -57,5 +71,5 @@ const renderBeers = (beer) => {
     }
 
     
-});
 
+})
